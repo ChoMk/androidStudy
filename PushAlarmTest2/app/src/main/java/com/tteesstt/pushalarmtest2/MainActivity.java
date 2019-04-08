97cf04254ad6f56510d853149291fe20f364ac3e
ChoMk <39166150+ChoMk@users.noreply.github.com>
2019-04-09 01:42:32 +0900

    노티피케이션 뭉치키

---
+++ PushAlarmTest2/app/src/main/java/com/tteesstt/pushalarmtest2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tteesstt.pushalarmtest2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart, btnEnd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart = (Button)findViewById(R.id.btn_start);
+        btnEnd = (Button)findViewById(R.id.btn_end);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                //intent.putExtra("id", "temp");
+                startService(intent);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
---
+++ PushAlarmTest2/app/src/main/java/com/tteesstt/pushalarmtest2/MyService.java
@@ -0,0 +1,102 @@
+package com.tteesstt.pushalarmtest2;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyService extends Service {
+    NotificationManagerCompat  notificationManager;
+    ServiceThread thread;
+    Notification notification;
+    Notification notification2;
+
+    Notification summary;
+    NotificationChannel notificationChannel;
+    int tempID = 0;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+
+        notification = new NotificationCompat.Builder(getApplicationContext(),"testte")
+                .setContentTitle("명기님")
+                .setContentText("보냄")
+                .setSmallIcon(R.drawable.ic_announcement_black_24dp)
+                .setGroup("test")
+                .build();
+
+
+        notification2 = new NotificationCompat.Builder(getApplicationContext(), "testte")
+                .setContentTitle("보성님")
+                .setContentText("보냄")
+                .setSmallIcon(R.drawable.ic_announcement_black_24dp)
+                .setGroup("test")
+                .build();
+
+        summary = new NotificationCompat.Builder(getApplicationContext(), "testte")
+                .setContentText("no read")
+                .setSmallIcon(R.drawable.ic_announcement_black_24dp)
+                .setGroup("test")
+                .setGroupSummary(true) // summary noti 로 만드는 핵심, API 20 이상
+                .build();
+
+
+
+
+        myServiceHandler handler = new myServiceHandler();
+
+        thread = new ServiceThread(handler);
+        thread.start();
+
+
+        return START_STICKY;
+    }
+
+    //서비스가 종료될 때 할 작업
+
+    public void onDestroy() {
+        thread.stopForever();
+        thread = null;//쓰레기 값을 만들어서 빠르게 회수하라고 null을 넣어줌.
+    }
+
+    class myServiceHandler extends Handler {
+        @Override
+        public void handleMessage(android.os.Message msg) {
+
+
+            notificationManager.notify(tempID+1, notification);
+            //notificationManager.notify(0, notification2);
+            notificationManager.notify(0, summary);
+            tempID++;
+            Toast.makeText(MyService.this, "뜸?", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
+}
